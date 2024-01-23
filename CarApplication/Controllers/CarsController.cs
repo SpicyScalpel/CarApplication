5c982d61ee9e077ebf1dd9093876a9a044a8156f
karina <karina.kulakova.97@gmail.com>
2024-01-23 12:25:25 +0200

    CarCreate

---
+++ CarApplication/Controllers/CarsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarApplication.Controllers
+{
+    public class CarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
