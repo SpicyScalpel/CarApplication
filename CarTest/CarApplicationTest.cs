ad76b921b6e3be79770b4aa89a2d30bc5a352428
karina <karina.kulakova.97@gmail.com>
2024-01-25 17:40:01 +0200

    Added 4 tests in CarTest, filled TestBase but CarTest project still in progress

--- CarTest/CarApplicationTest.cs
+++ CarTest/CarApplicationTest.cs
@@ -1,12 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Car.Core.Dto;
+using Car.Core.ServiceInterface;
 
 namespace CarTest
 {
-    internal class CarApplicationTest
+    public class CarApplicationTest : TestBase
     {
+        [Fact]
+        public async Task ShouldNot_AddEmptyCar_WhenReturnResult()
+        {
+            CarsDto dto = new CarsDto();
+
+            dto.CarBrand = "Toyota";
+            dto.CarModel = "C-HR";
+            dto.CarYear = 2024;
+            dto.CarColor = "Gray";
+            dto.CarPrice = 32000;
+            dto.CreatedAt = DateTime.Now;
+            dto.UpdatedAt = DateTime.Now;
+
+            var result = await Svc<ICarServices>().Create(dto);
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Should_DeleteByIdCar_WhenDeleteCar()
+        {
+            //Arrange
+            CarsDto car = MockCarData();
+
+            //Act
+            var addCar = await Svc<ICarServices>().Create(car);
+            var result = await Svc<ICarServices>().Delete((Guid)addCar.Id);
+
+            //Assert
+            Assert.Equal(result, addCar);
+        }
+
+        [Fact]
+        public async Task ShouldNot_DeleteByIdCar_WhenDidNotDeleteTheWrightCar()
+        {
+            CarsDto car = MockCarData();
+
+            var addCar = await Svc<ICarServices>().Create(car);
+            var addCar2 = await Svc<ICarServices>().Create(car);
+
+            var result = await Svc<ICarServices>().Delete((Guid)addCar2.Id);
+
+            Assert.NotEqual(result.Id, addCar.Id);
+        }
+
+        [Fact]
+        public async Task ShouldNot_UpdateCar_WhenNotUpdateDatata()
+        {
+            CarsDto dto = MockCarData();
+            await Svc<ICarServices>().Create(dto);
+
+            CarsDto nullUpdate = MockNullCar();
+            await Svc<ICarServices>().Update(nullUpdate);
+
+            var nullId = nullUpdate.Id;
+
+            Assert.True(dto.Id == nullId);
+        }
+
+        private CarsDto MockCarData()
+        {
+            CarsDto car = new()
+            {
+                CarBrand = "BMW",
+                CarModel = "iX2",
+                CarYear = 2023,
+                CarColor = "red",
+                CarPrice = 85700,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+            };
+
+            return car;
+        }
+
+        private CarsDto MockNullCar()
+        {
+            CarsDto nullDto = new()
+            {
+                Id = null,
+                CarBrand = "Audi",
+                CarModel = "A6",
+                CarYear = 2024,
+                CarColor = "Black",
+                CarPrice = 58325,
+                CreatedAt = DateTime.Now.AddYears(1),
+                UpdatedAt = DateTime.Now.AddYears(1),
+            };
+
+            return nullDto;
+        }
     }
 }
--- CarTest/TestBase.cs
+++ CarTest/TestBase.cs
@@ -1,11 +1,61 @@
+using CarTest.Macros;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Car.Core.ServiceInterface;
+using Car.ApplicationServices.Services;
+using Car.Data;
+
 namespace CarTest
 {
-    public class TestBase
+    public abstract class TestBase : IDisposable
     {
-        [Fact]
-        public void Test1()
-        {
+        protected IServiceProvider serviceProvider { get; }
 
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        protected T Svc<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+
+        protected T Macro<T>() where T : IMacros
+        {
+            return serviceProvider.GetService<T>();
+        }
+
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddScoped<ICarServices, CarServices>();
+
+            services.AddDbContext<CarContext>(x =>
+            {
+                x.UseInMemoryDatabase("TEST");
+                x.ConfigureWarnings(e => e.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            });
+
+            RegisterMacros(services);
+        }
+
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macroBaseType = typeof(IMacros);
+            var macros = macroBaseType.Assembly.GetTypes()
+                .Where(x => macroBaseType.IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract);
+
+            foreach (var macro in macros)
+            {
+                services.AddSingleton(macro);
+            }
         }
     }
 }
